1fe9ef7a0d8958adad90c6c85789291755a13e17
Francisco Mendoza González <alu0100600582@ull.edu.es>
2015-11-26 20:15:34 +0100

    Añadiendo las respuestas en modo array

--- models/respuesta.js
+++ models/respuesta.js
@@ -1,18 +1,33 @@
-"use stric";
-
 function Respuesta(respuesta){
-  this.respuesta = respuesta;
+
 
 
   if(typeof(this.respuesta) === 'string' || typeof(this.respuesta) === 'number') {
     return function(x){return x === respuesta;};
   }
-  else if (this.respuesta.__proto__.toString() === '/(?:)/') {
+
+  else if(respuesta instanceof RegExp){
     return function(x){return x.match(respuesta);};
   }
-  else if (this.respuesta.__proto__.toString() === "function () {}") {
-    return respuesta;
-  }
+
+  else if(respuesta instanceof Array){
+     return function(x){
+       if(respuesta.length != x.length) return false;
+
+       var objeto = true;
+
+       for(var i=0; i<respuesta.length; i++){
+         if(respuesta[i] != x[i]) objeto = false;
+       }
+
+       return objeto;
+     };
+   }
+
+   else {
+     return respuesta;
+   }
+
 }
 
 
